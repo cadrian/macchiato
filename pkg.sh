981e5f40d6a8d38aee1311610b860cb6f8052316
Cyril Adrian <cyril.adrian@gmail.com>
2025-05-29 20:46:57 +0200

    fix debian package creation

--- pkg.sh
+++ pkg.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 cd Macchiato
-debuild -us -uc -b
+mvn package || exit 1
+
+cd ..
+
+rm -rf Macchiato.deb
+cp -a Macchiato Macchiato.deb
+
+cd Macchiato.deb
+mv pom.xml pom-orig.xml
+mv pom-debian.xml pom.xml
+exec debuild -us -uc -b
